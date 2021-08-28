57489f10210d21ece2b276207a137f6606878e1d
unknown <bechen1@berkeley.edu>
2021-08-27 23:51:05 -0700

    write result function, start queueing PQ

--- hw4/bearmaps/hw4/AStarSolver.java
+++ hw4/bearmaps/hw4/AStarSolver.java
@@ -5,6 +5,7 @@ import bearmaps.proj2ab.ArrayHeapMinPQ;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import edu.princeton.cs.algs4.Stopwatch;
 
 public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
 
@@ -16,22 +17,54 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
     HashMap<Vertex, Double> disTo;
     HashMap<Vertex, WeightedEdge<Vertex>> edgeTo;
     ArrayList<WeightedEdge<Vertex>> solutionPath;
+    double constructorTime;
 
 
 
     AStarSolver(AStarGraph<Vertex> input, Vertex start, Vertex end, double timeOut) {
+        Stopwatch s = new Stopwatch();
         this.start = start;
         this.end = end;
         this.timeOut = timeOut;
         this.input = input;
         this.disTo = new HashMap<>();
         this.edgeTo = new HashMap<>();
-        solutionPath = new ArrayList<>();
+        this.pq = new ArrayHeapMinPQ<>();
+        this.solutionPath = new ArrayList<>();
+        this.constructorTime = 0;
+
+        pq.add(start, 0 + input.estimatedDistanceToGoal(start, end));
+
+        while (pq.size() > 0 || s.elapsedTime() < timeOut || pq.getSmallest().equals(end)) {
+            for (WeightedEdge<Vertex> e : input.neighbors(pq.removeSmallest())) {
+                relax(e);
+                }
+            }
+        }
+
 
     }
 
     SolverOutcome result() {
-        return null;
+        if (this.pq.getSmallest().equals(this.end)) {
+            return SolverOutcome.SOLVED;
+        } else if (this.pq.size() == 0) {
+            return SolverOutcome.UNSOLVABLE;
+        } else {
+            return SolverOutcome.TIMEOUT;
+        }
+    }
+
+    void relax(WeightedEdge<Vertex> e) {
+        /** Take into account input.estimatedDistance
+         * potentialNewDistance = distanceToP + edgeWeight + heurisitc (q to end)
+         * heuristic should not be added in disTo (should be completely separate)
+         * distance from start to p + edgeWeight < current distance to q
+         *      update distance to q to p + edgeweight
+         *      changepriority q or add q to PQ with priority = potentialNewDistance
+         *
+         * **/
+
     }
 
     public SolverOutcome outcome();
