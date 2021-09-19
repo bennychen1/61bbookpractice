a8acc18c22b51148eef871b1e64d50ceb732b314
unknown <bechen1@berkeley.edu>
2021-09-18 21:07:51 -0700

    Rewrite speed test

--- proj2c/bearmaps/test/TestAugmentedStreetMapGraph.java
+++ proj2c/bearmaps/test/TestAugmentedStreetMapGraph.java
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 
 public class TestAugmentedStreetMapGraph {
     private static final String OSM_DB_PATH = "../library-sp19/data/proj2c_xml/berkeley-2019.osm.xml";
+    private static final String OSM_DB_PATH_SMALL = "../library-sp19/data/proj2c_xml/berkeley-2019-small.osm.xml";
+    private static final String OSM_DB_PATH_TINY = "../library-sp19/data/proj2c_xml/tiny-clean.osm.xml";
     private static final double minLat = 37.8228;
     private static final double maxLat = 37.8922;
     private static final double minLon = -122.2997999;
@@ -39,31 +41,31 @@ public class TestAugmentedStreetMapGraph {
         assertEquals(location3, a.closest(lon4, lat4));
     }
 
+    // Speed - N is the number of points in the graph, not number of nearest calls
     @Test
     public void testSpeed() {
 
-        AugmentedStreetMapGraph a = new AugmentedStreetMapGraph(OSM_DB_PATH);
+        String[] points = new String[]{OSM_DB_PATH_TINY, OSM_DB_PATH_SMALL, OSM_DB_PATH};
+        double[] time = new double[points.length];
+
 
         Random rLat = new Random();
         Random rLon = new Random();
+        double[][] latLon = new double[1000][2];
 
-        int[] numPoints = new int[]{10, 100, 1000, 10000, 100000, 1000000};
-        double[] time = new double[numPoints.length];
+        for (int i = 0; i < latLon.length; i = i + 1) {
+            double lon = minLon + (maxLon - minLon) * rLon.nextDouble();
+            double lat = minLat + (maxLat - minLat) * rLat.nextDouble();
 
-        for (int i = 0; i < numPoints.length; i = i + 1) {
-            int numRandomPoints = numPoints[i];
-            double[][] latLon = new double[numRandomPoints][2];
-            for (int j = 0; j < numRandomPoints; j = j + 1) {
-                double lon = minLon + (maxLon - minLon) * rLon.nextDouble();
-                double lat = minLat + (maxLat - minLat) * rLat.nextDouble();
+            latLon[i][0] = lon;
+            latLon[i][1] = lat;
+        }
 
-                latLon[j][0] = lon;
-                latLon[j][1] = lat;
-            }
-
+        for (int i = 0; i < points.length; i = i + 1) {
+            AugmentedStreetMapGraph a = new AugmentedStreetMapGraph(points[i]);
             Stopwatch s = new Stopwatch();
-            for (int k = 0; k < numRandomPoints; k = k + 1) {
-                a.closest(latLon[k][0], latLon[k][1]);
+            for (int j = 0; j < latLon.length; j = j + 1) {
+                a.closest(latLon[j][0], latLon[j][1]);
             }
 
             time[i] = s.elapsedTime();
