fa2bbf91663616136214510caca75a61a40c5db9
unknown <bechen1@berkeley.edu>
2021-10-02 13:54:26 -0700

    set up FlightSolver skeleton code

---
+++ Lab8Challenge/Flight.java
@@ -0,0 +1,11 @@
+public class Flight {
+    int start;
+    int end;
+    int numPassengers;
+
+    public Flight(int start, int end, int numPassengers) {
+        this.start = start;
+        this.end = end;
+        this.numPassengers = numPassengers;
+    }
+}
---
+++ Lab8Challenge/FlightSolver.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class FlightSolver {
+    List<Flight> flights;
+
+    public FlightSolver(List<Flight> flights) {
+        this.flights = flights;
+    }
+
+    /** Returns the minutes that has the most passengers in flight. **/
+    public int solve() {
+        return 0;
+    }
+
+}
