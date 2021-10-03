cbbecfaae6415bd63047b242fead97835c95a28d
unknown <bechen1@berkeley.edu>
2021-10-03 11:30:37 -0700

    write solve method

--- Lab8Challenge/FlightSolver.java
+++ Lab8Challenge/FlightSolver.java
@@ -1,4 +1,7 @@
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.HashMap;
+import java.util.Comparator;
 
 public class FlightSolver {
     List<Flight> flights;
@@ -7,9 +10,74 @@ public class FlightSolver {
         this.flights = flights;
     }
 
-    /** Returns the minutes that has the most passengers in flight. **/
+    /** Returns the largest number of passengers that are in flight at once. **/
     public int solve() {
-        return 0;
+        PriorityQueue<Flight> startPQ = new PriorityQueue<>(10, new FlightComparatorStart());
+        PriorityQueue<Flight> endPQ = new PriorityQueue<>(10, new FlightComparatorEnd());
+
+        for (Flight f : flights) {
+            startPQ.add(f);
+            endPQ.add(f);
+        }
+
+        int curMax = 0;
+        int curTotal = 0;
+
+        while (startPQ.size() != 0 && endPQ.size() != 0) {
+
+            int curEnd = endPQ.peek().end;
+            int curStart = startPQ.peek().start;
+
+            if (curStart <= curEnd) {
+                curTotal = curTotal + startPQ.poll().numPassengers;
+                if (curTotal > curMax) {
+                    curMax = curTotal;
+                }
+            } else {
+                curTotal = curTotal + -1 * endPQ.poll().numPassengers;
+            }
+        }
+
+        return curMax;
+    }
+
+    class FlightComparatorStart implements Comparator<Flight> {
+        @Override
+        public int compare(Flight a, Flight b) {
+            if (a.start < b.start) {
+                return -1;
+            } else if (a.start == b.start) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    }
+
+    class FlightComparatorEnd implements Comparator<Flight> {
+        @Override
+        public int compare(Flight a, Flight b) {
+            if (a.end < b.end) {
+                return -1;
+            } else if (a.end == b.end) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
     }
 
 }
+
+// Record the start number of passengers, then end time is negative that number of passengers
+// Each end time is an interval and then find the max within that interval
+// Keep track of the flight duration
+// PQ of times sorted by number of passengers, then return the max.
+// <start1, start2, end1, start3, end2, end3> - each end is an interval to put in PQ
+    // end1 = start1+start2, end2 = start3, end3 = start3 + end2
+// don't need to return the minute with the most passengers
+// Two PQs - one with start as priority and one with end as priority
+    // poll endPQ and then remove min from startPQ and add passengers until the start time is over the endPQ polled time
+    //.have a variable tracking max seen.
+    // subtract endPQ polled passengers from total
+    // make sure startPQ is not empty - go until startPQ is empty
--- Lab8Challenge/TestFlightSolver.java
+++ Lab8Challenge/TestFlightSolver.java
@@ -1,9 +1,85 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
+import edu.princeton.cs.algs4.Stopwatch;
+
 
 public class TestFlightSolver {
     @Test
     public void testFlightSovler() {
+        Flight flight1 = new Flight(1, 5, 10);
+        Flight flight2 = new Flight(2, 7, 3);
+        Flight flight3 = new Flight(4, 8, 15);
+        Flight flight4 = new Flight(1, 2, 1);
+        Flight flight5 = new Flight(9, 10, 1);
+
+        ArrayList<Flight> flights = new ArrayList<>();
+
+        flights.add(flight1);
+        flights.add(flight2);
+        flights.add(flight3);
+        flights.add(flight4);
+        flights.add(flight5);
+
+        FlightSolver f = new FlightSolver(flights);
+
+        assertEquals(28, f.solve());
+    }
+
+    @Test
+    public void testFlightSolverMultiple() {
+        Flight flight1 = new Flight(5, 10, 30);
+        Flight flight2 = new Flight(2, 9, 15);
+        Flight flight3 = new Flight(5, 8, 20);
+        Flight flight4 = new Flight(1, 10, 9);
+        Flight flight5 = new Flight(4, 10, 3);
+        Flight flight6 = new Flight(1, 3, 50);
+
+        ArrayList<Flight> flights = new ArrayList<>();
+
+        flights.add(flight1);
+        flights.add(flight2);
+        flights.add(flight3);
+        flights.add(flight4);
+        flights.add(flight5);
+        flights.add(flight6);
+
+        FlightSolver f = new FlightSolver(flights);
+
+        assertEquals(77, f.solve());
+    }
+
+    @Test
+    public void testSpeed() {
+
+        Random r = new Random(10);
+
+        int[] numFlights = new int[]{10, 100, 1000, 10000, 100000, 1000000};
+        double[] time = new double[numFlights.length];
+
+        for (int i = 0; i < numFlights.length; i = i + 1) {
+            int curNumFlights = numFlights[i];
+            ArrayList<Flight> flights = new ArrayList<>();
+
+            for (int j = 0; j < curNumFlights; j = j + 1) {
+                flights.add(new Flight(100 + (int)(Math.random() * ((500 - 100) + 1)),
+                        1000 + (int)(Math.random() * (6000 - 1000) + 1),
+                        100 + (int)(Math.random() * (600 - 100 + 1))));
+            }
+
+            FlightSolver f = new FlightSolver(flights);
+
+            Stopwatch s = new Stopwatch();
+
+            f.solve();
+
+            time[i] = s.elapsedTime();
+        }
+
+        System.out.println(Arrays.toString(time));
 
     }
 }
