4f8807254575c2249775b7b6ff548669794e442c
unknown <bechen1@berkeley.edu>
2022-01-15 18:58:12 -0800

    getRandomPoint

--- proj3/byow/Core/RandomMap.java
+++ proj3/byow/Core/RandomMap.java
@@ -3,10 +3,7 @@ package byow.Core;
 import byow.TileEngine.TETile;
 import byow.TileEngine.Tileset;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 
 /** An object representing a randomly generated map. **/
 public class RandomMap {
@@ -26,29 +23,43 @@ public class RandomMap {
     /** The tile array that represents this map. */
     TETile[][] tileArray;
 
-    /** Instantiate a basic 30x30 map. */
-    RandomMap() {
+    /** The random number generator for this map. */
+    Random ran;
+
+    /** The width (number of columns) of the map. */
+    int width;
+
+    /** The length (number of rows) of the map. */
+    int length;
+
+    /** Instantiate a basic 30x30 map. Provide a seed for randomness */
+    RandomMap(int seed) {
         this.roomList = new ArrayList<>();
         this.hallwayList = new ArrayList<>();
         this.roomQueue = new LinkedList<>();
         this.hallwayQueue = new LinkedList<>();
         this.tileArray = new TETile[30][30];
         this.helperPopulateMap(this.tileArray, Tileset.NOTHING);
+        this.ran = new Random(seed);
+        this.width = 30;
+        this.length = 30;
     }
 
-    /** Instantiate a w by l map where w is width and l is the length.
+    /** Instantiate a w by l map where w is width and l is the length. Provide seed for randomness.
      * w is number of columns, which is the number of arrays, l is the number of rows, which is the length of
      * the inner arrays.
      * */
-    RandomMap(int w, int l) {
-        this();
+    RandomMap(int w, int l, int seed) {
+        this(seed);
         this.tileArray = new TETile[w][l];
+        this.width = w;
+        this.length = l;
         this.helperPopulateMap(this.tileArray, Tileset.NOTHING);
     }
 
     /** Instantiate a w by l map where w is the width and l is the length. Specify default tile type to start with. */
-    RandomMap(int w, int l, TETile tileType) {
-        this(w, l);
+    RandomMap(int w, int l, TETile tileType, int seed) {
+        this(w, l, seed);
         this.helperPopulateMap(this.tileArray, tileType);
     }
 
@@ -78,7 +89,7 @@ public class RandomMap {
         // go back to room queue, find possible connections, add connections, ...
         // only add to queues if valid to avoid infinite loops
         // have a seen hashSet
-        // have whole thing be a graph? 
+        // have whole thing be a graph?
         /**
          * for each room in roomQ
          *      pick a room - mark it so it doesn't get revisited again.
@@ -87,6 +98,7 @@ public class RandomMap {
          *              pick a hall - mark it so it doesn't get revisited again.
          *              find possible connections, add hall, room, or nothing.
          */
+
     }
 
     /** Add a room to the map.
@@ -116,6 +128,26 @@ public class RandomMap {
     /** Helper to find the closest size room if given room can't be drawn at a point. */
     private Room largestValidRoom(Room r) {
         return null;
+    }
+
+    /** Choose a random point to create a connection. */
+    private Point helperRandomPoint(List<Point> listOfPoints) {
+        // pick a random point from a list.
+        return null;
+    }
+
+    /** Instantiates a room of random width and length at a point. */
+    private Room helperRandomRoom(Point p) {
+        // largestValidRoom?
+        return null;
+    }
+
+    /** Pick a random point on the map to draw the initial room . */
+    private Point getRandomPoint() {
+
+        int rowIndex = RandomUtils.uniform(ran, this.length- 1);
+        int colIndex = RandomUtils.uniform(ran, this.width - 1);
+        return new Point(rowIndex, colIndex);
     }
 
 
