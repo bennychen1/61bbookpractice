c854613575929280a23915e6b4181f9dc42e9c76
unknown <bechen1@berkeley.edu>
2022-01-24 22:20:07 -0800

    add point list

--- proj3/byow/Core/Room.java
+++ proj3/byow/Core/Room.java
@@ -1,5 +1,7 @@
 package byow.Core;
 
+import java.util.ArrayList;
+
 /** Represents a room with width, length, lower left corner, .... **/
 public class Room {
 
@@ -12,18 +14,21 @@ public class Room {
     /** The lower left corner of the room. **/
      Point start;
 
+     /** A list of points that comprise the room. To be populated when drawn. */
+     ArrayList<Point> roomPoints;
+
      /** Instantiate a room of width w and length l that starts at Point p. **/
      public Room(int w, int l, Point p) {
          this.width = w;
          this.length = l;
          this.start = p;
+         this.roomPoints = new ArrayList<>();
      }
 
      /** Instantiate a default room of 0 width and 0 length at (0, 0). **/
      public Room() {
-         this.width = 0;
-         this.length = 0;
-         this.start = new Point(0, 0);
+         this(0, 0, new Point(0, 0));
      }
+
 
 }
