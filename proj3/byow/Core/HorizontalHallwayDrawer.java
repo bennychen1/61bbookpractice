11910444975b087376ea2b3df8c4e72fb59f95d2
unknown <bechen1@berkeley.edu>
2022-01-30 20:57:23 -0800

    add startRoom param to interface

--- proj3/byow/Core/HorizontalHallwayDrawer.java
+++ proj3/byow/Core/HorizontalHallwayDrawer.java
@@ -6,16 +6,16 @@ import byow.TileEngine.Tileset;
 public class HorizontalHallwayDrawer implements HallwayDrawer{
 
     @Override
-    public void draw(RandomMap m, Point startingPoint, int numTiles) {
+    public void draw(RandomMap m, Room startRoom, Point startingPoint, int numTiles) {
         int startingCol = startingPoint.col;
         int startingRow = startingPoint.row;
 
         for (int curCol = startingCol; curCol < startingCol + numTiles; curCol = curCol + 1) {
             TETile[] curTileArray = m.getTileArray()[curCol];
             if (!curTileArray[startingRow].equals(Tileset.FLOOR)) {
-                curTileArray[startingRow] = Tileset.FLOOR;
-                this.roomSets.union(helper2DIndexConvertor(startRoom.start),
-                        helper2DIndexConvertor(curCol, startingRow));
+                m.setTileArray(curCol, startingRow, Tileset.FLOOR);
+                m.unionPoints(m.helper2DIndexConvertor(startRoom.start),
+                        m.helper2DIndexConvertor(curCol, startingRow));
             }
         }
     }
