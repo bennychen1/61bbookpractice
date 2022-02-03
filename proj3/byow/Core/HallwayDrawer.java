d94324e831a1d51443b0d9dd4cfccbf084eeac13
unknown <bechen1@berkeley.edu>
2022-02-02 23:27:28 -0800

    move RandomMap horizontal hallyway code to the interface, change interface args

--- proj3/byow/Core/HallwayDrawer.java
+++ proj3/byow/Core/HallwayDrawer.java
@@ -1,9 +1,14 @@
 package byow.Core;
 
 public interface HallwayDrawer {
-    /** Draws a hallway numTiles long either vertically or horizontally from point startPoint.
-     * Size includes p1.
-     *
+    /** Draws a hallway of random length between p1 and p2.
+     * Returns the last point of the drawn hallway.
+     * @param m     A RandomMap where the hallway will be drawn.
+     * @param r1    A random Room in the map
+     * @param p1    A random Point in Room r1
+     * @param r2    A random Room in the map.
+     * @param p2    A random point in Room r2.
      */
-    public void draw(RandomMap m, Room startRoom, Point startPoint, int numTiles);
+    public Point draw(RandomMap m, Room r1, Point p1, Room r2,
+                     Point p2);
 }
--- proj3/byow/Core/HorizontalHallwayDrawer.java
+++ proj3/byow/Core/HorizontalHallwayDrawer.java
@@ -6,9 +6,29 @@ import byow.TileEngine.Tileset;
 public class HorizontalHallwayDrawer implements HallwayDrawer{
 
     @Override
-    public void draw(RandomMap m, Room startRoom, Point startingPoint, int numTiles) {
-        int startingCol = startingPoint.col;
-        int startingRow = startingPoint.row;
+    public Point draw(RandomMap m, Room r1, Point p1,
+                     Room r2, Point p2) {
+
+        Point startPoint;
+        Room startRoom;
+
+        if (p1.col < p2.col) {
+            startPoint= p1;
+            startRoom = r1;
+
+        } else {
+            startPoint = p2;
+            startRoom = r2;
+        }
+
+
+        int startingCol = startPoint.col;
+        int startingRow = startPoint.row;
+
+        int maxWidth = p1.horizontalDistance(p2);
+
+        int numTiles = RandomUtils.uniform(m.ran, 1, maxWidth + 2);
+
 
         for (int curCol = startingCol; curCol < startingCol + numTiles; curCol = curCol + 1) {
             TETile[] curTileArray = m.getTileArray()[curCol];
