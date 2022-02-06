9482d47d557b06c789e0d11dac7c0069b8524249
unknown <bechen1@berkeley.edu>
2022-02-06 00:12:40 -0800

    add test for point directions

--- proj3/byow/Core/TestEngine.java
+++ proj3/byow/Core/TestEngine.java
@@ -12,14 +12,33 @@ public class TestEngine {
 
     @Test
     public void testDrawTwoRooms() {
-        RandomMap m = new RandomMap(70, 30, Tileset.SAND, 20);
+        RandomMap m = new RandomMap(70, 30, Tileset.SAND, 10);
 
         m.drawWorld();
 
 
-        TETile[][] tileArray = m.tileArray;
+        TETile[][] tileArray = m.getTileArray();
         TERenderer t = new TERenderer();
         t.initialize(70, 31);
+        t.renderFrame(tileArray);
+
+        int i = 0;
+
+        while (i == 0) {
+            continue;
+        }
+    }
+
+    @Test
+    public void testDrawHallways() {
+        RandomMap m = new RandomMap(20, 15, Tileset.SAND, 10);
+
+        m.drawWorld();
+
+
+        TETile[][] tileArray = m.getTileArray();
+        TERenderer t = new TERenderer();
+        t.initialize(20, 15);
         t.renderFrame(tileArray);
 
         int i = 0;
@@ -44,6 +63,25 @@ public class TestEngine {
     public void testUnion() {
         RandomMap m = new RandomMap(5,5, 1);
         m.drawWorld();
+    }
+
+    @Test
+    public void testPoint() {
+        Point p = new Point(5, 6);
+
+        Point left = p.pointToLeft();
+        Point right = p.pointToRight(10);
+        Point rightNull = p.pointToRight(5);
+        Point leftUpDiagonal = p.pointLeftUpDiagonal(7);
+        Point rightUpDiagonal = p.pointRightUpDiagonal(7, 6);
+
+        assertNull(rightNull);
+        assertEquals(4, left.col);
+        assertEquals(6, right.col);
+        assertEquals(4, leftUpDiagonal.col);
+        assertEquals(7, leftUpDiagonal.row);
+        assertEquals(6, rightUpDiagonal.col);
+        assertEquals(7, rightUpDiagonal.row);
     }
 
 }
