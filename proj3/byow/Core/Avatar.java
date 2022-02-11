4d146837548199afcf854369863495ae18373abc
unknown <bechen1@berkeley.edu>
2022-02-10 23:22:27 -0800

    get started, write constructor

--- proj3/byow/Core/Avatar.java
+++ proj3/byow/Core/Avatar.java
@@ -1,2 +1,23 @@
-package byow.Core;public class Avatar {
+package byow.Core;
+
+import byow.TileEngine.TETile;
+
+import java.awt.Color;
+
+
+public class Avatar extends TETile {
+
+    /** A Point representing the current location of the avatar on the map. */
+    private Point location;
+
+
+    Avatar() {
+        this('@', new Point(0, 0));
+    }
+
+    Avatar(char icon, Point location) {
+        super(icon, Color.white, Color.black, "avatar");
+        this.location = location;
+    }
+
 }
