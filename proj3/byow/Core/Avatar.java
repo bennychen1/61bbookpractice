155461b719adeb979c7b42b779d80d8dcb372c37
unknown <bechen1@berkeley.edu>
2022-02-10 23:37:37 -0800

    add getter and setter

--- proj3/byow/Core/Avatar.java
+++ proj3/byow/Core/Avatar.java
@@ -4,20 +4,34 @@ import byow.TileEngine.TETile;
 
 import java.awt.Color;
 
-
+/** A Tile that represents the avatar. **/
 public class Avatar extends TETile {
 
     /** A Point representing the current location of the avatar on the map. */
     private Point location;
 
 
+    /** The default constructor for the avatar. **/
     Avatar() {
         this('@', new Point(0, 0));
     }
 
+    /**
+     * Instantiate an avatar displayed as the provided char that will be placed on the specified location
+     * on a map.
+     * @param icon  how the avatar will be shown on a map.
+     * @param location A Point representing where this will be initially placed on a map.
+     */
     Avatar(char icon, Point location) {
         super(icon, Color.white, Color.black, "avatar");
         this.location = location;
     }
+
+    /** A getter method to get the Point location of the avatar. **/
+    public Point getLocation() {
+        return new Point(this.location.)
+    }
+
+    /** A setter method to set
 
 }
