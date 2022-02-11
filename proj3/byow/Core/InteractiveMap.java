12cada80389f07f94573cdad9dc1b18745848e4f
unknown <bechen1@berkeley.edu>
2022-02-10 23:37:58 -0800

    write constructor

--- proj3/byow/Core/InteractiveMap.java
+++ proj3/byow/Core/InteractiveMap.java
@@ -1,2 +1,24 @@
-package byow.Core;public class InteractiveMap {
+package byow.Core;
+
+import jh61b.junit.In;
+
+/** A RandomMap that is interactive. **/
+public class InteractiveMap {
+
+    /** A RandomMap representing the game map. **/
+    private RandomMap gameMap;
+
+    /** The avatar of the user. **/
+    private Avatar userAvatar;
+
+    /**
+     * The full constructor for InteractiveMap.
+     * @param m A RandomMap object representing the game map.
+     * @param a An Avatar for the user.
+     */
+    InteractiveMap(RandomMap m , Avatar a) {
+        this.gameMap = m;
+        this.userAvatar = a;
+    }
+
 }
