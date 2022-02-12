b2026c0cd03a6d0d3f3102590e1aea63c2a645e8
unknown <bechen1@berkeley.edu>
2022-02-11 23:10:37 -0800

    write placeAvatar, isPointValid, originalTile variable

--- proj3/byow/Core/InteractiveMap.java
+++ proj3/byow/Core/InteractiveMap.java
@@ -1,5 +1,6 @@
 package byow.Core;
 
+import byow.TileEngine.TETile;
 import jh61b.junit.In;
 
 /** A RandomMap that is interactive. **/
@@ -11,6 +12,9 @@ public class InteractiveMap {
     /** The avatar of the user. **/
     private Avatar userAvatar;
 
+    /** The original tile that the Avatar took the place of. */
+    private TETile originalTile;
+
     /**
      * The full constructor for InteractiveMap.
      * @param m A RandomMap object representing the game map.
@@ -19,6 +23,27 @@ public class InteractiveMap {
     InteractiveMap(RandomMap m , Avatar a) {
         this.gameMap = m;
         this.userAvatar = a;
+        this.originalTile = null;
+
+        if (isPointValid(this.userAvatar.getLocation())) {
+            placeAvatar();
+        }
+    }
+
+    /**
+     * Returns true if p is a valid location on the game map.
+     * @param p A Point object to check
+     * @return  True if p is a valid point on the map, false otherwise.
+     */
+    public boolean isPointValid(Point p) {
+        return (p.getRow() >= 0 && p.getCol() >= 0) && (p.getRow() < gameMap.getMaxRowIndex() &&
+                p.getCol() < gameMap.getMaxColIndex());
+    }
+
+    /** Put the avatar in its location in the tile array of the game map.  **/
+    public void placeAvatar() {
+        this.originalTile = this.gameMap.getTileAt(this.userAvatar.getLocation());
+        this.gameMap.setTileArray(this.userAvatar.getLocation(), this.userAvatar);
     }
 
 }
