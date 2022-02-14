6fbef1f3593c3614cd625d7309638b5faff294c4
unknown <bechen1@berkeley.edu>
2022-02-13 16:46:29 -0800

    add location setting and consumedTile

--- proj3/byow/Core/Avatar.java
+++ proj3/byow/Core/Avatar.java
@@ -10,11 +10,14 @@ public class Avatar extends TETile {
     /** A Point representing the current location of the avatar on the map. */
     private Point location;
 
+    /** The tile that the avatar took the place of. **/
+    private TETile consumedTile;
 
     /** The default constructor for the avatar. **/
     Avatar() {
         this('@', new Point(0, 0));
     }
+
 
     /**
      * Instantiate an avatar displayed as the provided char that will be placed on the specified location
@@ -35,9 +38,22 @@ public class Avatar extends TETile {
     /**
      * A setter method to set the location for this avatar
      * @param col  the column index to set the location to.
-     * @param row  the row index to set the location to. 
+     * @param row  the row index to set the location to.
      */
     public void setLocation(int col, int row) {
+        this.location = new Point(col, row);
+    }
 
+    /**
+     * Move to the provided location.
+     * @param location A Point representing a location.
+     */
+    public void setLocation(Point location) {
+        this.location = location;
+    }
+
+    /** Return the tile the avatar took the place of. **/
+    public TETile getConsumedTile() {
+        return this.consumedTile;
     }
 }
