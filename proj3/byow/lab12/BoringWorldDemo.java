2db55e592569b40eae2c364f207395abdad11257
unknown <bechen1@berkeley.edu>
2022-02-16 23:30:01 -0800

    hard code to test out

--- proj3/byow/lab12/BoringWorldDemo.java
+++ proj3/byow/lab12/BoringWorldDemo.java
@@ -24,6 +24,9 @@ public class BoringWorldDemo {
             }
         }
 
+        world[2][0] = Tileset.AVATAR;
+
+
         // fills in a block 14 tiles wide by 4 tiles tall
         for (int x = 20; x < 35; x += 1) {
             for (int y = 5; y < 10; y += 1) {
