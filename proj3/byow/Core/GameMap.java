2b16888377a7e61a6e09d30839cbaea9e0d8b2e1
unknown <bechen1@berkeley.edu>
2022-03-08 00:17:55 -0800

    add common functions

--- proj3/byow/Core/GameMap.java
+++ proj3/byow/Core/GameMap.java
@@ -1,2 +1,11 @@
-package byow.Core;public interface GameMap {
+package byow.Core;
+
+import java.util.List;
+
+/** A grouping for the types of game maps. **/
+public interface GameMap {
+    public boolean isPlaying();
+    public RandomMap getGameMap();
+    public List<InteractiveMap.Avatar> getAvatarList();
+
 }
--- proj3/byow/Core/GameType.java
+++
@@ -1,7 +0,0 @@
-package byow.Core;
-
-/** The type of game being played. **/
-public interface GameType {
-
-    public boolean isPlaying(GameMap m);
-}
